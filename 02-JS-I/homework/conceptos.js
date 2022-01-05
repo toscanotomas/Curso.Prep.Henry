c63a91e27551d6cba3101fde1293199c53f3b810
tomas <casa@casa.localdomain>
2022-01-04 21:33:41 -0300

    Primera tarea completa

---
+++ 02-JS-I/homework/conceptos.js
@@ -0,0 +1,23 @@
+## 1
+## Variables: Es una palabra clave el cual sirve para asignarle 
+## un valor y poder guardar datos, mayormente usando la 
+## palabra var, un espacio y el nombre de la variable.
+
+## Strings: Es una cadena de caracteres que pueden ser
+## palabras, simbolos o numeros que se utilizan en una 
+## oracion y definen entre comillas, ej: ('').
+
+## Funciones: Son obejetos los cuales nos permiten dividir el trabajo 
+## que hace un programa en tareas mas pequeñas.
+## Los argumentos son varibles que agramos dentro de la funcion.
+## return es una instruccion en la cual los datos escaṕan de 
+## una funcion y nos devuelve su valor en la consola.
+
+## Declaraciones: Son una serie de intrucciones con las 
+## cuales se realizan condiciones,se incluyen dentro 
+## de las funciones, ej: if.
+
+## Valores booleanos: Proviene del codigo binario (0, 1) 
+## es el lenguaje con el cual se comunican las computadoras
+## en las que se utilizan dos opciones, verdadero o falso
+## ej: (true) o (false).
