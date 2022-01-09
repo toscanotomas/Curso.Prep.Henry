74289f6fe01708931fc1c10fc95c61138bdae574
tomas <casa@casa.localdomain>
2022-01-09 18:12:42 -0300

    Segunda tarea

---
+++ 03-JS-II/homework/conceptos.js
@@ -0,0 +1,16 @@
+1
+## for: Es un bucle que se utiliza cuando queremos
+## repetir una accion o conjunto de intruccion un numero
+## determinado de veces.map.
+
+## &&: Es un operador logico que se usa cuando
+## hay dos o mas argumentos y devolvera un true
+## en caso de que sean verdaderas o false si una
+## opcion es falsa.&& significa 'and'.
+
+## ||: Es un operador logico que significa or y
+## si una de las expresiones es true es por que son
+## verdaderas o false si ambas expresiones son falsas.
+
+## !: Es un operador logico not se utiliza para negar
+## una igualdad y devolver el valor contrario.
