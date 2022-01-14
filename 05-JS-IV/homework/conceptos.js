d087e8e22cd2c8d143d37ae3ad617f9e91feb902
tomas <casa@casa.localdomain>
2022-01-14 16:07:00 -0300

    Cuarta tarea

---
+++ 05-JS-IV/homework/conceptos.js
@@ -0,0 +1,14 @@
+//Objetos: Es un conjunto de elementos que pueden contener 
+//variables, metodos y propiedades.
+//
+//Propiedades: Es un relacion entre una clave y un valor asociado.
+//Por ejemplo nombre: henrry.
+//
+//Metodos: Es una funcion la cual esta dentro de un objeto.
+//
+//Bucles: Es una forma para realizar tareas repetitivas.
+//
+//Notación de puntos vs notación de corchetes: Son formas
+//de acceder rapido a las propiedades y metodos de un objeto.
+//Mientras que el primero se utiliza para cualquier tipo
+//de propiedade el segundo es para las strings.
