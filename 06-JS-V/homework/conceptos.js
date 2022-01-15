20cd43357ce3fca522ef4c7f22135bdced5b4e10
tomas <casa@casa.localdomain>
2022-01-15 02:41:50 -0300

    Quinta tarea

---
+++ 06-JS-V/homework/conceptos.js
@@ -0,0 +1,8 @@
+// Prototype: Es una objeto que usa como propiedades alternativas
+// de un otro objeto.Es una propiedad de Object (objeto general) en
+// javascript.
+//
+// Contructors: Es un tipo de objeto general que usa para especificar
+// una clase de objeto, por ejemplo: persona.Cada objeto que se crea
+// derivando de este se llama instacia, que un objeto particular, por
+// ejemplo,pedro.
